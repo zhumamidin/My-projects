6276bbfea34b46d2c1005304c0b69bd1b459236a
Zhumamidin Zholdoshov <zhumamidin.zholdoshov@gmail.com>
2024-01-17 16:01:47 +0545

    added new sections and styles

--- Xiaomi Himo/js/main.js
+++ Xiaomi Himo/js/main.js
@@ -1,9 +1,9 @@
 $(function () {
 	$('.bike__slider').slick({
+		autoplay: true,
+		autoplaySpeed: 2000,
 		arrows: false,
 		dots: true,
 		fade: true,
-		autoplay: true,
-		autoplaySpeed: 2000,
 	})
 })
