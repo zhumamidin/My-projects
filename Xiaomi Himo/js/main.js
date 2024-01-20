cfa16553ff75587fca53e341cfa744b5cd266bdb
Zhumamidin Zholdoshov <zhumamidin.zholdoshov@gmail.com>
2024-01-20 14:08:52 +0545

    finished the project

--- Xiaomi Himo/js/main.js
+++ Xiaomi Himo/js/main.js
@@ -6,4 +6,11 @@ $(function () {
 		dots: true,
 		fade: true,
 	})
+
+	$('.header__menu, .bike__column').on('click', 'a', function (event) {
+		event.preventDefault();
+		let id = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	})
 })
